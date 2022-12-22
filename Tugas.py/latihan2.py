62d8fa41e3c43294d7b209c6dae27323dfb76979
sejahteradamai41 <115250576+sejahteradamai41@users.noreply.github.com>
2022-12-22 10:06:32 +0700

    Add files via upload

---
+++ Tugas.py/latihan2.py
@@ -0,0 +1,6 @@
+J = int(input("masukan input:"))
+X = str("#")
+for i in range(0,J):
+    for j in range(0,7):
+        print(X,end=" ")
+print()
---
+++ Tugas.py/latihan3.py
@@ -0,0 +1,9 @@
+#input
+a = float(input("Masukan banyaknya angka :")) 
+b = float(input("masukan angka:"))
+c = float(input("masukan angka:"))
+d = float(input("masukan angka:"))
+e = float(input("masukan angka:"))
+f = float(input("masukan angka:"))
+hasil = (18+12.5+(-4.2)+123.9+(-2))/6 
+print("hasil rata-rata perhitungan nilai adalah:"+str(hasil))
